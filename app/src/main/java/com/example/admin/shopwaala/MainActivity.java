a93c51cd5a4fda669b409642b860fdec9bebe7ce
saiteja <saiteja.rangi0615@gmail.com>
2017-02-27 16:41:57 +0530

    user sign in page

--- app/src/main/java/com/example/admin/shopwaala/MainActivity.java
+++ app/src/main/java/com/example/admin/shopwaala/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.admin.shopwaala;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ET_NAME,ET_PASS;
+    String login_name,login_pass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sree0);
+
+        ET_NAME = (EditText)findViewById(R.id.username);
+        ET_PASS = (EditText)findViewById(R.id.password);
+
+
+    }
+
+    public void userReg(View view){
+        startActivity(new Intent(this,Register.class));
+
+    }
+
+    public void userLogin(View view){
+        login_name = ET_NAME.getText().toString();
+        login_pass = ET_PASS.getText().toString();
+        String method = "login";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method,login_name,login_pass);
+
+
+
+
     }
 }
